45f360f31e82b31c0db4eec3d1b67572882112d0
Карим Башароа <karimbasharov@gmail.com>
2021-01-26 22:08:49 +0200

    Добавьте файлы проекта.

---
+++ MvvmApp/MvvmApp/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MvvmApp.Models;
+using MvvmApp.Views;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmApp
+{
+    public partial class App : Application
+    {
+        public const string DATABASE_NAME = "friends2.db";
+        public static FriendsAsyncRepository database;
+        public static FriendsAsyncRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new FriendsAsyncRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MvvmApp/MvvmApp/FriendPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MvvmApp.Models;
+using MvvmApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FriendPage : ContentPage
+    {
+        public FriendPage()
+        {
+            InitializeComponent();
+        }
+        private async void SaveFriend(object sender, EventArgs e)
+        {
+            var friend = (Friend)BindingContext;
+            if (!String.IsNullOrEmpty(friend.Name))
+            {
+                await App.Database.SaveItemAsync(friend);
+            }
+            await this.Navigation.PopAsync();
+        }
+        private async void DeleteFriend(object sender, EventArgs e)
+        {
+            var friend = (Friend)BindingContext;
+            await App.Database.DeleteItemAsync(friend);
+            await this.Navigation.PopAsync();
+        }
+        private async void Cancel(object sender, EventArgs e)
+        {
+            await this.Navigation.PopAsync();
+        }
+    }
+}
